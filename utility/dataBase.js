58330d7a1a4169490950e5596fae016512604ff5
devam262004d <vaghanidevam262004@gmail.com>
2025-07-04 17:29:23 +0530

    first commit

--- utility/dataBase.js
+++ utility/dataBase.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/interview");
+    await mongoose.connect("mongodb+srv://admin:e3yCfqZgD6.ves6@cluster0.jkuesug.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("✅ MongoDB connected successfully!");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
