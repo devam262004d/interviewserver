a3af17370bfa9e62ccee7a00c095a3843d7131f6
devam262004d <vaghanidevam262004@gmail.com>
2025-07-04 16:38:35 +0530

    first commit

--- utility/dataBase.js
+++ utility/dataBase.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/interview", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true, 
-    });
+    await mongoose.connect("mongodb://localhost:27017/interview");
     console.log("✅ MongoDB connected successfully!");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
